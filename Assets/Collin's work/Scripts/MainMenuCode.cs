d4e35ebbcac69d714fc0817e4351bd6010a69346
testmaster217 <55296006+testmaster217@users.noreply.github.com>
2020-03-07 11:53:06 -0500

    Added a skeleton of a main menu.

---
+++ Assets/Collin's work/Scripts/MainMenuCode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuCode : MonoBehaviour
+{
+    // The sub-menus.
+    public Canvas titleMenu;
+    public Canvas tutorialMenu;
+
+    // This is if play button is clicked (starts the game).
+    public void playGame()
+    {
+        // Change the scene name for the actual scene.
+        SceneManager.LoadSceneAsync("CollinTestScene");
+    }
+    // This is if how to play button is clicked (toggles the 
+    // tutorial screen).
+    public void toggleTutorial()
+    {
+        if (tutorialMenu.gameObject.activeSelf)
+        {
+            tutorialMenu.gameObject.SetActive(false);
+            titleMenu.gameObject.SetActive(true);
+        }
+        else
+        {
+            tutorialMenu.gameObject.SetActive(true);
+            titleMenu.gameObject.SetActive(false);
+        }
+    }
+    // This is if quit button is clicked (quits the game).
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
