1fe9b197f1a9985ace6b8876d730ecbec7c5faf7
Izzy Bennett <buildgamesnotbombs@gmail.com>
2020-03-08 16:33:04 -0400

    Map Completed

--- Assets/IzzyStuff/Scripts/SignCode.cs
+++ Assets/IzzyStuff/Scripts/SignCode.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SignCode : MonoBehaviour
 {
 
     
     public string inputString;
+    public Text signText;
+    public Text topText;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +26,8 @@ public class SignCode : MonoBehaviour
     {
         inputString = input;
         Debug.Log("Changed island name to:" + inputString);
+        topText.text = "This Island is called:";
+        signText.text = inputString + " :)";
+        this.gameObject.SetActive(false);
     }
 }
